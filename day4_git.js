09e5c738b0e97f394bf667d94876a24aa4e7109d
Nilesh Yadav <ny172005@gmail.com>
2025-12-26 18:46:51 +0530

    Newcommit

--- day4_git.js
+++ day4_git.js
@@ -4,6 +4,26 @@ console.log(button);
 const login="Login Page added"
 console.log(login)
 
+// |_|->|_|
+// button -> login 
 
+const payment= "Integrated the payment gateway"
+console.log(payment)
+
+// (master)|_|->|_|
+//               |
+//              |_|(payment)
+
+const upi="Integrated the upi also"
+console.log(upi);
+
+// (master)|_|->|_|
+//               |
+//              |_|(payment)->upi
 const footer="footer addded in our websute"
 console.log(footer)
+
+// rebasing mein yahi cheez
+
+// (master)|_|->|_|-> |_|(payment)->upi
+
